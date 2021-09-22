c31ef918ed98d7fdeee3049bb3636139464fb40e
LIMITLESS-IO <anas.raed2002@gmail.com>
2021-09-22 23:56:33 +0300

    added a new font ,edited the text n resized the picture in the center
    added adventpro font, edited the text's size and formatting and added a few lines distancing it from the svg in the background , resized the svg picture in the background , built an initial build to deploy it on the host with text telling the visitors that the site is under the development then removed the text from the source code.

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:techgate/Screens/Welcome/components/background.dart';
+import 'package:techgate/constants.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -12,8 +13,22 @@ class Body extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text(
-            "Welcome to Tech Gate JU",
-            style: TextStyle(fontWeight: FontWeight.bold),
+            '''Welcome to Tech Gate JU
+            
+
+
+
+
+            
+            ''',
+            textAlign: TextAlign.center,
+            maxLines: 15,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: kPrimaryColor,
+              fontSize: 24,
+              fontFamily: 'advent',
+            ),
           ),
           SvgPicture.asset(
             "assets/icons/rise.svg",
