234f529bb922c315185b70cedcfcbf671cd43e9f
LIMITLESS-IO <anas.raed2002@gmail.com>
2021-09-25 20:07:37 +0300

    this is pure pain
    struggling with firebase auth , I realized it nuked my app build n have to throw the effort on an auth service in the trash , struggled for a few hours with initializing firebase , following their official docs I eventually got it working , now I have to code a loading screen for when someone is connecting to the site , and a "something has gone wrong" screen for whenever an error occurs , as well as working on the auth service
    I'd start with the auth service since I've already figured what I'd probably be doing

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,34 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:techgate/Screens/Welcome/welcome_screen.dart';
 import 'package:techgate/constants.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TechGate LogIn',
-      theme: ThemeData(
-        primaryColor: kPrimaryColor,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: WelcomeScreen(),
+      future: _initialization,
+      builder: (context, snapshot) {
+        return MaterialApp(
+          title: "TechGate WelcomePage",
+          theme: ThemeData(
+              primaryColor: kPrimaryColor,
+              scaffoldBackgroundColor: Colors.white),
+          home: WelcomeScreen(),
+        );
+      },
     );
   }
 }
