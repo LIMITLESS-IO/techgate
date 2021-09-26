ff52be14c5f5e4625c34865ff3068bced2220f8f
LIMITLESS-IO <anas.raed2002@gmail.com>
2021-09-26 18:15:29 +0300

    I freaking DID ITTTTT
    okay , firebase and the authentication system are both working by now
    however , I have to make statements in the code for policies for passwords so one wouldn't put a single letter as a password lol
    and a way to validate email so hi@there.lol wouldn't be a valid email
    + email verification  as well

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,36 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:techgate/Screens/Welcome/welcome_screen.dart';
 import 'package:techgate/constants.dart';
+import 'package:firebase/firebase.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(MyApp());
+  initializeApp(
+      apiKey: "AIzaSyBHssw5ES6dxT0bg2-WN_xu-nu1RpPO3Is",
+      authDomain: "techgateju.firebaseapp.com",
+      databaseURL:
+          "https://techgateju-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "techgateju",
+      storageBucket: "techgateju.appspot.com",
+      messagingSenderId: "935506698832",
+      appId: "1:935506698832:web:bfb207d2259681fcb8e755",
+      measurementId: "G-13FN4XR1NN");
+  runApp(TechGate());
 }
 
-class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
+class TechGate extends StatelessWidget {
+  const TechGate({Key? key}) : super(key: key);
 
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        return MaterialApp(
-          title: "TechGate WelcomePage",
-          theme: ThemeData(
-              primaryColor: kPrimaryColor,
-              scaffoldBackgroundColor: Colors.white),
-          home: WelcomeScreen(),
-        );
-      },
+    return MaterialApp(
+      title: 'TechGate LogIn',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
     );
   }
 }
