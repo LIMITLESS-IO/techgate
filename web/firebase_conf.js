5a6cccaab86a11f8246f90d0c92d0fcb6a3780d7
LIMITLESS-IO <anas.raed2002@gmail.com>
2021-10-05 19:16:06 +0300

    hid firebase from the main html file for security

---
+++ web/firebase_conf.js
@@ -0,0 +1,12 @@
+export // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyBHssw5ES6dxT0bg2-WN_xu-nu1RpPO3Is",
+  authDomain: "techgateju.firebaseapp.com",
+  databaseURL: "https://techgateju-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "techgateju",
+  storageBucket: "techgateju.appspot.com",
+  messagingSenderId: "935506698832",
+  appId: "1:935506698832:web:bfb207d2259681fcb8e755",
+  measurementId: "G-13FN4XR1NN",
+};
